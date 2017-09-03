a55c937b5cc24716f40edfd928aa8ca61e58d94b
philipl12 <philiplin512@gmail.com>
2017-09-03 13:24:37 -0400

    Small loop
    Made an easy while loop.

--- scores.cpp
+++ scores.cpp
@@ -2,15 +2,17 @@
 #include <string>
 using namespace std;
 
-int main(int argc, char const *argv[]) {
+int main() {
     string input = "";
-    cout << "Input outcome of race: ";//must be 65 - 90
-    cin >> input;
-    for (int i = 0; i < input.length(); i++) {
-        if (input[i] < 65 || input[i] > 90) {
-            break;
+    while (input != "done") {
+        cout << "Input outcome of race: ";//must be 65 - 90
+        cin >> input;
+        for (int i = 0; i < input.length(); i++) {
+            if (input[i] < 65 || input[i] > 90) {
+                break;
+            }
+            cout << int(input[i]) << endl;
         }
-        cout << int(input[i]) << endl;
     }
 
     return 0;
