75b87f67871b9a79e8134620bca37d1b9927a8ce
philipl12 <philiplin512@gmail.com>
2017-09-10 21:05:38 -0400

    One step closer
    Made a function to reset the array ( not very efficient).

--- HW2.cpp
+++ HW2.cpp
@@ -1,30 +1,42 @@
 #include <iostream>
-#include <string>
+#include <string.h>
+#include <array>
 using namespace std;
+
+void reset(int a[], int n) {
+    for (int i = 0; i < n ; i++) {
+        a[i] = 0;
+    }
+}
 
 int main() {
     string input = "";
     int j = 0, array[91] = {0};
-    while (input != "done") {
-        cout << "Input outcome of race or done: ";//must be 65 - 90
+    while (input != "done") { //loops the input process
+        cout << "Input outcome of race or done: ";
         cin >> input;
         if (input == "done") { return 0; }
-        while (j < input.length()) {
+        while (j < input.length()) { //use this to go through string array
+            //purposely start array at 65 so that
+            //array[i] corresponds with input[j]
             for (int i = 65; i < 91; i++) {
-            /*    if (input[j] < 'A' || input[j] > 'Z') {
-                    cout << "You didn't enter an uppercase. Try again.\n";
-                    break;
-                }
-            */
                 if(input[j] == i) {
                     array[i]++;
-                    cout << i << " " << array[i] << endl;
                     j++;
                 }
             }
         }
+
+        //this section does the job of printing team results
+        int teamNum = 0;
+        for (int i = 65; i < 91; i++) {
+            if (array[i] == 0) { continue; }
+            teamNum = array[i];
+        }
+
         input = "";
         j = 0;
+        reset(array, 91);
     }
 
     return 0;
