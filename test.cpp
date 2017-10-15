1b104831c1aa768668981c37f41177bbb8e8cafc
philipl12 <philiplin512@gmail.com>
2017-10-15 18:19:53 -0400

    Board drawing
    Tried out a program that "draws" a board in console. Needs some tweaks.

--- test.cpp
+++ test.cpp
@@ -1,68 +1,58 @@
 #include <iostream>
-#include <cstdlib>
-#include <cmath>
 using namespace std;
 
+int main() {
+    int i, j, k, l;
 
-bool ok(int q[], int col){//method returns true if a queen can go in the spot. returns false if it cant
-    for(int i=0; i<col; i++)
-        if(q[col]==q[i] || (col-i)==abs(q[col]-q[i]))
-            return false;
-    return true;
-};//end of ok
+    typedef char box [5][7];
 
+    box bb, wb, *board[8][8];
 
-void backtrack(int &col){
-    col--;//this method goes back one column.
-    if(col==-1){//If it goes before all the columns, then all solutions have been found so the program ends
-        system("PAUSE");
-        exit(1);
+    //fills in board with respective "colors"
+    for (i = 0; i < 5; i++) {
+        for (j = 0; j < 7; j++) {
+            wb[i][j] = ' ';
+            bb[i][j] = char(219);
+        }
     }
-};//end of backtrack
 
-void print(int q[]){//when called the array called will be printed out
-    static int count =0;
-    cout<< ++ count<<endl<<endl;
-    int i;
-    for(i=0; i<8; i++)
-        cout<<q[i]<<" ";
-    cout<<endl<<endl;
-};//end of print
+    //fill board with pointers to respective colors
+    for (i = 0; i < 8; i++) {
+        for (j = 0; j < 8; j++) {
+            if ((i + j) % 2 == 0) { board [i][j] = &wb; }
+            else { board [i][j] = &bb; }
+        }
+    }
 
-int main(){
-    int q[8]; q[0]=0;
-    int c=1;
+    //prints board with pointers to respective colors
 
-    bool from_backtrack=false;//this variable will be used to determine whether the row should be reset to -1 in that column
+    //upper border
+    cout << "    ";
+    for (i = 0; i < 7*8; i++) {
+        cout << char(196);
+    }
+    cout << endl;
 
-    while(true){//the loop will repeat forever
-        while(c<8){//this should repeat 8 times for all the columns
+    //board actual
+    for (i = 0; i < 8; i++) {
+        for (k = 0; k < 5; k++) {
+            cout << "   " << char(179); //left border
 
-            if(!from_backtrack)q[c]=-1;//if this was reached where baktrack was not called then q[c] is set to -1
-            from_backtrack=true;
+            for (j = 0; j < 8; j++) {
+                for (l = 0; l < 7; l++) {
+                    cout << (*board[i][j])[k][l];
+                }
+            }
+            cout << char(179) << endl;
+        }
+    }
 
-            while(q[c]<8){//this loop repeats 8 times for the rows
-                q[c]++;
+    //lower border
+    cout << "    ";
+    for (i = 0; i < 7*8; i++) {
+        cout << char(196);
+    }
+    cout << endl;
 
-                if(q[c]==8){//if it reaches 8, then there is no solution, so backtrack is called
-                    from_backtrack=true;
-                    backtrack(c);
-                    break;
-                }//end of if(q[c]==8)
-
-                if(ok(q,c)){//this tests if the spot can have a queen. if it does then we go to the next column
-                    from_backtrack=false;
-                    c++;
-                    break;
-                }//end of if(ok(q,c)
-
-            }//end of while(q[c]<8)
-
-        }//end of while(c<8)
-        print(q);// By this point a solution was reached so we print it
-        from_backtrack=true;
-        backtrack(c);//now to find other solutions, we backtrack
-    }//end of while(true)
-
-
-}//end of main()
+    return 0;
+}
