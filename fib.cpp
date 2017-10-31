7355007a904dc4edcb0fd5189d1d9586c4e868f9
philipl12 <philiplin512@gmail.com>
2017-10-31 18:05:29 -0400

    Fib
    Limited to 47, going any higher will result in overflow error.

--- fib.cpp
+++ fib.cpp
@@ -2,19 +2,14 @@
 using namespace std;
 
 unsigned long long fib(unsigned long long n) {
-    if (n == 1 || n == 2) { return 1; }
-    return fib(n - 1) + fib(n - 2);
-}
-/*
-unsigned long long fib(unsigned long long n) {
-    static int memo[1000] = {0};
+    static int memo[100] = {0};
     if (n == 1 || n == 2) { return 1; }
     if (memo[n] != 0) return memo[n];
-    return neni[x] = fib(n - 1) + fib(n - 2);
+    return memo[n] = fib(n - 1) + fib(n - 2);
 }
-*/
+
 int main() {
-    for (unsigned long long i = 0; i < 70; i++) {
+    for (unsigned long long i = 1; i <= 47; i++) {
         cout << "fib ("<<i<<") = " << fib(i) << endl;
     }
     return 0;
