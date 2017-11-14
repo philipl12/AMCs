c77bac8f5896a0f9150b4b5c74f04073dea81944
philipl12 <philiplin512@gmail.com>
2017-11-13 20:24:45 -0500

    Towers need to move
    Have to figure out assignment of from and to.

--- towers.cpp
+++ towers.cpp
@@ -16,40 +16,37 @@ int main() {
         t[0].push_back(i);
     }
     t[1].push_back(n + 1);
-    t[2].push_back(n + 2);
+    t[2].push_back(n + 1);
 
     while (t[1].size() < n + 1) { // while t[1] doesn't contain all rings
         cout << "Move #" << ++move << ": Transfer ring " << candidate
              << " from tower " << char (from + 'A') << " to tower "
              << char (to + 'A') << "\n";
-    }
+        // Move the ring from the "from tower" to the "to tower"
+        // first copy it, then delete it form the "from tower"
 
-    // Move the ring from the "from tower" to the "to tower"
-    // irst copy it, then delete it form the "from tower"
+        // from = index of tower w/ smallest ring that hasn't just been moved
+        // (to + 1) % 3 or (to + 2) % 3
+        if (to % 2 == 1) {
+            from = (to + 2) % 3;
+        }
+        else {
+            from = (to + 1) % 3;
+        }
 
+        // candidate = ring on top of the t[from] tower
+        candidate = t[from].back();
 
-
-    // from = index of tower w/ smallest ring that hasn't just been moved
-    // (to + 1) % 3 or (to + 2) % 3
-    if () {
-        from = ;
-    }
-    else {
-        from = ;
-    }
-
-    // candidate = ring on top of the t[from] tower
-    candidate = ;
-
-    // to = the index of closest tower on which candidate can be placed
-    // (from + 1) % 3 or (from + 2) % 3
-    // compare candidate w/ the ring on closer tower; which tower is "closer"
-    // depends on whether n is odd or even
-    if () {
-        to = ;
-    }
-    else {
-        to = ;
+        // to = the index of closest tower on which candidate can be placed
+        // (from + 1) % 3 or (from + 2) % 3
+        // compare candidate w/ the ring on closer tower; which tower is "closer"
+        // depends on whether n is odd or even
+        if () {
+            to = ;
+        }
+        else {
+            to = ;
+        }
     }
     return 0;
 }
