ee25ba388295ab92ea764e502aa7bc7d697c2f4c
philipl12 <philiplin512@gmail.com>
2018-09-03 16:21:27 -0400

    File I/O works

--- main.cpp
+++ main.cpp
@@ -5,33 +5,39 @@
 using namespace std;
 
 int main(int argc, char** argv) {
+    int letters[256];
+    ifstream fileIn;
+    char myChar;
+
+    for (int i = 0; i < 256; i++) {
+        letters[i] = 0;
+    }
     /*
     cout << "It has " << argc << " arguments:" << endl;
     for (int i = 0; i < argc; ++i) {
         cout << argv[i] << endl;
     }
-
-    FILE * pFile;
-    char buffer[10000];
-
-    pFile = fopen (argv[1] , "r");
-    if (pFile == NULL) perror ("Error opening file");
-    else {
-        while ( ! feof (pFile) ) {
-            if ( fgets (buffer , 10000 , pFile) == NULL ) break;
-            fputs (buffer , stdout);
-        }
-    fclose (pFile);
-    }
     */
-    ifstream fileIn;
-    fileIn.open(argv[1]);
+    for (int i = 1; i < arc; i++) {
+        fileIn.open(argv[i]);
 
-    char myChar;
+        while (!fileIn.eof()) {
+            fileIn.get(myChar);
 
-    while (!fileIn.eof()) {
-        fileIn.get(myChar);
-        cout << myChar << endl;
+            for (int i = 0; i < 256; i++) {
+                if ((int)myChar == i) {
+                    letters[i]++;
+                    break;
+                }
+            }
+
+        }
+
+        for (int i = 32; i < 256; i++) {
+            if (letters[i] > 0) {
+                cout << (char)i << "   " << letters[i] << endl;
+            }
+        }
     }
 
     return 0;
