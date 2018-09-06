9e72e93e0762ee134c0047319c3058ba2e5637e9
philipl12 <philiplin512@gmail.com>
2018-09-06 14:39:16 -0400

    Correction for output

--- main.cpp
+++ main.cpp
@@ -1,44 +1,40 @@
 #include <iostream>
 #include <fstream>
-#include <stdio.h>
 
 using namespace std;
 
 int main(int argc, char** argv) {
     int letters[256];
     ifstream fileIn;
+    ofstream fileOut;
     char myChar;
 
     for (int i = 0; i < 256; i++) {
         letters[i] = 0;
     }
-    /*
-    cout << "It has " << argc << " arguments:" << endl;
-    for (int i = 0; i < argc; ++i) {
-        cout << argv[i] << endl;
-    }
-    */
-    for (int i = 1; i < arc; i++) {
-        fileIn.open(argv[i]);
 
-        while (!fileIn.eof()) {
-            fileIn.get(myChar);
+    fileIn.open(argv[1]);
+    fileOut.open(argv[2]);
 
-            for (int i = 0; i < 256; i++) {
-                if ((int)myChar == i) {
-                    letters[i]++;
-                    break;
-                }
-            }
+    while (!fileIn.eof()) {
+        fileIn.get(myChar);
 
-        }
-
-        for (int i = 32; i < 256; i++) {
-            if (letters[i] > 0) {
-                cout << (char)i << "   " << letters[i] << endl;
+        for (int i = 0; i < 256; i++) {
+            if ((int)myChar == i) {
+                letters[i]++;
+                break;
             }
         }
+
     }
 
+    for (int i = 33; i < 256; i++) {
+        if (letters[i] > 0) {
+            fileOut << (char)i << "   #" << letters[i] << endl;
+        }
+    }
+    
+    fileIn.close();
+    fileOut.close();
     return 0;
 }
